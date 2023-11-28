c6289eefa2b6e37cf1ee88624ba7c555f10d7170
iamthepoe <luca1901p@gmail.com>
2023-11-28 19:00:24 -0300

    feat: create hash service

---
+++ src/modules/hash/hash.service.js
@@ -0,0 +1,25 @@
+export default class HashService{
+	constructor(crypt){
+		this.crypt = crypt;
+	}
+
+	/**
+     * @public
+     * @param {string} text 
+     * @param {number} rounds 
+     * @returns {Promise<string>}
+     */
+	hash(text, rounds = 10){
+		return this.crypt.hash(text, rounds);
+	}
+
+	/**
+     * @public
+     * @param {string} text 
+     * @param {string} hash 
+     * @returns {Promise<boolean>}
+     */
+	compare(text, hash){
+		return this.crypt.compare(text, hash);
+	}
+}
