1bf0e66aa882df4319a163c1f8bb76e96bd86a9f
iamthepoe <luca1901p@gmail.com>
2023-11-29 19:39:32 -0300

    feat: add task runner for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import prettier from 'gulp-prettier';
+import gulpIf from 'gulp-if';
 import { exec } from 'child_process';
+
+let testPassed = false;
 
 async function test() {
   return new Promise((resolve, reject) => {
     exec('npm run test:unit', (error, stdout, stderr) => {
-      console.error(stderr);
+      console.error('FAILED TESTS:', stderr);
       if (error) {
         reject(error);
       } else {
+        testPassed = true;
         resolve();
       }
     });
   });
 }
 
-async function build() {
-  return gulp.src('src/**/*.js')
+gulp.task('default', gulp.series(test, (done) => {
+  gulp.src('src/**/*.js')
     .pipe(prettier())
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
-}
-
-gulp.task('default', gulp.series(test, build));
+    .pipe(gulpIf(testPassed, gulp.dest('dist')));
+  done();
+}));
