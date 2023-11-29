59820369a159135a39472643a25c2e284459eed1
iamthepoe <luca1901p@gmail.com>
2023-11-29 01:07:33 -0300

    feat: add babel to build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,22 +21,4 @@ module.exports = {
 		'ecmaVersion': 'latest',
 		'sourceType': 'module'
 	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
 };
--- test/app.e2e-spec.js
+++ test/app.e2e-spec.js
@@ -1,19 +1,42 @@
 import { it, describe, before, after } from 'node:test';
 import assert from 'node:assert';
+import process from 'node:process';
 import SetupServer from '../src/server/server.js';
+import { configDotenv } from 'dotenv';
 
-describe('App (e2e)', ()=>{
-	let server = new SetupServer();
+configDotenv();
 
-	before(()=>{
-		server.init();
-	});
+const PORT = process.env.SERVER_PORT;
 
-	after(()=>{
-		server.close();
-	});
+describe('App (e2e)', () => {
+  let server = new SetupServer();
 
-	it('should be defined', ()=>{
-		assert.ok(server);
-	});
+  before(() => {
+    server.init();
+  });
+
+  after(() => {
+    server.close();
+  });
+
+  it('should be defined', () => {
+    assert.ok(server);
+  });
+
+  it('should signup an user with success', async () => {
+    const user = {
+      name: 'João',
+      email: 'joao@escribo.com',
+      password: 'senhadojoao999',
+      phones: [{ ddd: '11', number: '99722821232' }],
+    };
+
+    const response = await fetch(`http://localhost:${PORT}/sign-up`, {
+      body: JSON.stringify(user),
+      method: 'POST',
+    });
+
+    assert.ok(response);
+    assert.strictEqual(response.status, 201);
+  });
 });
